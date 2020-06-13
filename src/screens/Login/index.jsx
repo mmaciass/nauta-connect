74ea33a79774d31497409cc5c204cae32ef94d3d
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-06-12 21:58:13 -0400

    Refactor and reindex code

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,33 +1,14 @@
 import React, { Fragment } from 'react';
 import { Form, Formik } from 'formik';
 import logo from '../../assets/img/icon-128.png';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { connect } from 'react-redux';
-import * as yup from 'yup';
 import TextFieldCustom from '../../components/TextFieldCustom';
 import ShareButtons from '../../components/SharedButtons';
-
-const useStyles = makeStyles((theme) => ({
-  logo: {
-    maxHeight: 80,
-    maxWidth: 80,
-    marginTop: 10,
-  },
-  logoContainer: {
-    textAlign: 'center',
-  },
-  buttonsContainer: {
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  ShareButtons: {
-    marginTop: 20,
-    textAlign: "center"
-  },
-}));
+import schemaValidation from './schemaValidation';
+import useStyles from './useStyles';
 
 const initialValue = {
   username: '',
@@ -35,19 +16,7 @@ const initialValue = {
   remember: false,
 };
 
-const schemaValidation = yup.object({
-  username: yup.string().required('Este campo es obligatorio').email('Este campo no coincide con una cuenta valida.')
-    .test('only-numbers', 'Este campo no coincide con una cuenta valida.',
-      (value) => {
-        return !!value && !!value.split('@')[1]
-          && (value.split('@')[1] === 'nauta.com.cu' || value.split('@')[1] === 'nauta.co.cu');
-      },
-    ),
-  password: yup.string().required('Este campo es obligatorio'),
-  remember: yup.boolean(),
-});
-
-const Login = ({ dispatch, ...props }) => {
+const Login = (props) => {
   const classes = useStyles();
   return (
     <Fragment>
@@ -56,18 +25,7 @@ const Login = ({ dispatch, ...props }) => {
       </div>
       <Formik initialValues={initialValue} onSubmit={(values, formikHelpers) => {
         formikHelpers.setSubmitting(true);
-        fetch('https://secure.etecsa.net:8443//LoginServlet', {
-          method: 'POST',
-          body: JSON.stringify({ username: values.username, password: values.password }),
-        })
-          .then(value => {
-            cconsole.log(value);
-            formikHelpers.setSubmitting(false);
-          })
-          .catch(reason => {
-            console.log(reason);
-            formikHelpers.setSubmitting(false);
-          });
+
       }} validationSchema={schemaValidation}>
         {(formikBag) => (
           <Form>
---
+++ src/screens/Login/useStyles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    maxHeight: 80,
+    maxWidth: 80,
+    marginTop: 10,
+  },
+  logoContainer: {
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  ShareButtons: {
+    marginTop: 20,
+    textAlign: 'center',
+  },
+}));
+
+export default useStyles;
