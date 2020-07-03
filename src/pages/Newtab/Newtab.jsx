35653abc89a840265a3d2be9de612272da44273e
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-07-02 23:26:41 -0400

    remove file from code

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
 import './Newtab.css';
 
 const Newtab = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
         </p>
