081687f5d62db2252b6e31d4ec89b983b09d556d
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-07-07 07:34:29 -0400

    fix error status var

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -6,7 +6,7 @@ import { saveUserAction } from './userStorageAction';
 
 const loginAction = (username, password, remember = false) => {
   return (dispatch) => {
-    dispatch({ type: 'LOGIN_BEGIN', payload: { state: 'loading', username } });
+    dispatch({ type: 'LOGIN_BEGIN', payload: { status: 'loading', username } });
     const bodyData = { username, password };
     fetchCustom('https://secure.etecsa.net:8443//LoginServlet', bodyData)
       .then(value => {
@@ -16,26 +16,26 @@ const loginAction = (username, password, remember = false) => {
         if (value.includes('Entre el nombre de usuario y contraseña correctos.' || value.includes('No se pudo autorizar al usuario.'
           || value.includes('El nombre de usuario o contraseña son incorrectos.')))) {
           chrome.runtime.sendMessage({ type: 'LOGIN_ERROR', payload: 'El nombre de usuario o contraseña son incorrectos.' });
-          dispatch({ type: 'LOGIN_FAILURE', payload: { state: 'error' } });
+          dispatch({ type: 'LOGIN_FAILURE', payload: { status: 'error' } });
         } else if (value.includes('El usuario ya está conectado.')) {
           chrome.runtime.sendMessage({ type: 'LOGIN_ERROR', payload: 'Ya se encuentra un usuario conectado.' });
-          dispatch({ type: 'LOGIN_FAILURE', payload: { state: 'error' } });
+          dispatch({ type: 'LOGIN_FAILURE', payload: { status: 'error' } });
         } else if (value.includes('Usted ha realizado muchos intentos.')) {
           chrome.runtime.sendMessage({
             type: 'LOGIN_ERROR',
             payload: 'Usted ha realizado muchos intentos. Por favor intente más tarde.',
           });
-          dispatch({ type: 'LOGIN_FAILURE', payload: { state: 'error' } });
+          dispatch({ type: 'LOGIN_FAILURE', payload: { status: 'error' } });
         } else if (value.includes('Su tarjeta no tiene saldo disponible.')) {
           chrome.runtime.sendMessage({
             type: 'LOGIN_ERROR',
             payload: 'Su cuenta no tiene saldo disponible.',
           });
-          dispatch({ type: 'LOGIN_FAILURE', payload: { state: 'error' } });
+          dispatch({ type: 'LOGIN_FAILURE', payload: { status: 'error' } });
         } else {
           const resp = dataWrapper(value);
           dispatch(updateTimeLeftAction());
-          dispatch({ type: 'LOGIN_SUCCESS', payload: { state: 'connected', ...resp } });
+          dispatch({ type: 'LOGIN_SUCCESS', payload: { status: 'connected', ...resp } });
           if (remember) dispatch(saveUserAction(username, password));
           dispatch(forceUpdateAction());
         }
@@ -43,7 +43,7 @@ const loginAction = (username, password, remember = false) => {
       .catch(reason => {
         if (reason.message === 'Failed to fetch')
           chrome.runtime.sendMessage({ type: 'LOGIN_ERROR', payload: 'Ha ocurrido un error con la conexión de red.' });
-        dispatch({ type: 'LOGIN_FAILURE', payload: { state: 'error' } });
+        dispatch({ type: 'LOGIN_FAILURE', payload: { status: 'error' } });
         console.log(reason);
       });
   };
