860659c9cc24d3c3b109356f7401bc9078407bb3
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-08-19 13:28:52 -0400

    fix errors with colors

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -6,7 +6,19 @@ import Connect from '../../screens/Connect';
 import useStyles from '../../screens/useStyles';
 import Splash from '../../screens/Splash';
 import { Box } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blue, indigo } from '@material-ui/core/colors';
 // import ShareButtons from '../../components/SharedButtons';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: blue,
+    type: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+  },
+});
 
 const Popup = ({ login, configs, ...props }) => {
   const classes = useStyles();
@@ -17,15 +29,19 @@ const Popup = ({ login, configs, ...props }) => {
   }, []);
 
   return (
-    <Box {...props}>
-      <Splash/>
-      <div style={{ paddingTop: 100 }}/>
-      {login.status === 'connected' ? <Connect/> : <Login/>}
-      {/*<Connect/>*/}
-      {/*<ShareButtons className={classes.ShareButtons}/>*/}
-      {/*<div style={{ marginBottom: 15 }}/>*/}
-      <NotifierMessenger/>
-    </Box>
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Box {...props}>
+        <Splash/>
+        <div style={{ paddingTop: 100 }}/>
+        {login.status === 'connected' ? <Connect/> : <Login/>}
+        {/*<Connect/>*/}
+        {/*<ShareButtons className={classes.ShareButtons}/>*/}
+        {/*<div style={{ marginBottom: 15 }}/>*/}
+        <NotifierMessenger/>
+      </Box>
+    </ThemeProvider>
   );
 };
 
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { blue, indigo } from '@material-ui/core/colors';
-import { ThemeProvider } from '@material-ui/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { Store } from 'react-chrome-redux';
 import { Provider } from 'react-redux';
 import { loginInitialState } from '../../store/reducers/login';
 import Popup from './Popup';
 import { configInitialState } from '../../store/reducers/configs';
 import { userStorageInitial } from '../../store/reducers/userStorage';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: indigo,
-    secondary: blue,
-    type: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
-  },
-});
 
 const proxyStore = new Store({
   state: {
@@ -32,10 +20,7 @@ const proxyStore = new Store({
 const App = (props) => {
   return (
     <Provider store={proxyStore}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <Popup/>
-      </ThemeProvider>
+      <Popup/>
     </Provider>
   );
 };
